5789826ab0a7005f8a66976c09eaaa5a1a5632c5
Marc Siegel <marc@usainnov.com>
2022-10-24 13:53:10 -0400

    WIP: Aruba steps based on copy-and-paste workaround

--- examples/aruba_hello_world/tests/cucumber_aruba_example.rs
+++ examples/aruba_hello_world/tests/cucumber_aruba_example.rs
@@ -1,34 +1,52 @@
+use std::process::Command;
 use aruba::cucumber_parameters::MaybeNot;
 use aruba::text::sanitize_text;
-use cucumber::{given, then, Parameter, World};
+use cucumber::{given, then, Parameter, World, when};
 
 // `World` is your shared, likely mutable state.
 // Cucumber constructs it via `Default::default()` for each scenario.
 #[derive(Debug, Default, World)]
 pub struct ArubaHelloWorld {
-    aruba: ArubaWorldState
+    aruba: aruba::prelude::ArubaWorldState
 }
 
-#[derive(Debug, Default)]
-pub struct ArubaWorldState {
-    dummy: u32
-}
+/// START COPY FROM ARUBA
+///
+/// Hello! Please copy the code from the above START COPY comment down to END COPY comment below,
+/// and place above the `main` function in a file such as `tests/cucumber_aruba_steps.rs`.
+///
+/// The unfortunate need to copy code is pending resolution of the following issue:
+///   * [cucumber-rs #225: How to write and publish reusable steps?](https://github.com/cucumber-rs/cucumber/issues/235)
+///
+/// Please be sure to vote and/or comment on that issue as we work towards resolution, thank you!
+mod copy_from_aruba {
+    use aruba::prelude::*;
 
-// #[when(regex = r"^I run `([^`]*)`$")]
-// fn run_and_wait_step(world: &mut ArubaHelloWorld) {
-//     world.aruba.run_and_wait
-// }
-
-#[then(expr = "the exit status should{maybe_not} be {int}")]
-fn exit_status_step(_world: &mut ArubaHelloWorld, should: MaybeNot, expected: u32) {
-    let status = 0;
-    if should.into() {
-        assert_eq!(status, expected);
-    } else {
-        assert_ne!(status, expected)
+    #[derive(Debug, Default, World)]
+    pub struct ArubaWorld {
+        aruba: ArubaWorldState
     }
 
+    #[when(regex = r"^I run `([^`]*)`$")]
+    pub fn aruba_when_run_and_wait(world: &mut ArubaWorld, command: String) {
+        world.aruba.when_run_and_wait(command.as_str());
+    }
 }
+/// END COPY FROM ARUBA
+
+// use aruba::step_definitions::run_and_wait_step;
+// impl ArubaWorld for ArubaHelloWorld {
+//     fn last_command_run(&self) -> &Option<Command> {
+//         &None
+//     }
+// }
+
+// impl Into<&mut ArubaWorld> for ArubaHelloWorld {
+//     fn into(&mut self) -> &mut ArubaWorld {
+//         &mut self.aruba
+//     }
+// }
+
 
 // This runs before everything else, so you can setup things here.
 fn main() {
--- examples/aruba_hello_world/tests/features/help.feature
+++ examples/aruba_hello_world/tests/features/help.feature
@@ -3,3 +3,4 @@ Feature: Help text
   Scenario: If we pass the `--help` flag, we see usage documentation
     When I run `aruba_hello_world --help`
     Then the exit status should be 0
+     And the stdout contains: "Usage: "
