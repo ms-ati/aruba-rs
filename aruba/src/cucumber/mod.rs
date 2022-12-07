1055818402a629845093cc635852e8a21bf092fa
Marc Siegel <marc@usainnov.com>
2022-12-07 13:40:51 -0500

    Rename world to aruba_world to match struct name

--- aruba/src/cucumber/mod.rs
+++ aruba/src/cucumber/mod.rs
@@ -1,7 +1,7 @@
+mod aruba_world;
 mod step_custom_parameters;
 mod step_definitions;
-mod world;
 
+pub use aruba_world::*;
 pub use step_custom_parameters::*;
 pub use step_definitions::*;
-pub use world::*;
