4625803f3d8d8787ed0be395e79703d3bb9044e0
Marc Siegel <marc@usainnov.com>
2022-12-07 13:29:35 -0500

    Rename cucumber_custom_parameters to step_custom_parameters

--- aruba/src/steps/mod.rs
+++ aruba/src/steps/mod.rs
@@ -1,7 +1,7 @@
 mod command_steps;
-mod cucumber_custom_parameters;
+mod step_custom_parameters;
 mod world;
 
 pub use command_steps::*;
-pub use cucumber_custom_parameters::*;
+pub use step_custom_parameters::*;
 pub use world::*;
